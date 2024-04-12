f599a8562af2dc3b8a8b734e8fccafac67d4768f
Mohamed Samy <m0hamedSamy>
2024-04-12 13:40:28 +0200

    Add Onboarding Screen to the navigation.

--- app/src/main/java/com/example/chatapp/main/ui/App.kt
+++ app/src/main/java/com/example/chatapp/main/ui/App.kt
@@ -2,13 +2,24 @@ package com.example.chatapp.main.ui
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.chatapp.main.domain.MainStartDestination
+import com.example.chatapp.main.ui.util.Screen
+import com.example.chatapp.onboarding.ui.OnboardingScreen
 
 @Composable
 fun App(startDestination: MainStartDestination) {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = startDestination.route){
+        composable(route = Screen.Onboarding.route) {
+            OnboardingScreen(
+                onSignUpClick = {  },
+                onLogInClick = {  },
+                onSignUpWithFacebookClick = {  },
+                onSignUpWithGoogleClick = {  },
+            )
+        }
     }
 }
