9d7a8a847c8d552e8b9799342362e2d104b8ce53
Mohamed Samy <m0hamedSamy>
2024-04-12 13:42:24 +0200

    Remove unused colors.

--- app/src/main/java/com/example/chatapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/chatapp/ui/theme/Color.kt
@@ -3,11 +3,7 @@ package com.example.chatapp.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val PrimaryGreen = Color(0xFF24786D)
-val MediumGreen = Color(0xFF20A090)
 val PrimaryBlack = Color(0xFF000E08)
 val PrimaryGrey = Color(0xFF797C7B)
-val LiteGrey = Color(0xFFF3F6F6)
 val PrimaryWhite = Color(0xFFFFFFFF)
-val PrimaryRed = Color(0xFFF04A4C)
-val LiteBabyBlue = Color(0xFFF2F7FB)
 val Transparent = Color(0x00FFFFFF)
